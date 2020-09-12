7d644ba0eae5e7512032c326c3ef7fd8adbb5779
mithunraj <mithun.raj159@gmail.com>
2020-09-12 14:32:36 +0530

    Finished Work

--- MapLocationApp/ViewController.swift
+++ MapLocationApp/ViewController.swift
@@ -7,13 +7,47 @@
 //
 
 import Cocoa
+import MapKit
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: NSTextField!
+    
+    var locationManager = CLLocationManager()
+    var recogniser: NSClickGestureRecognizer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = 10
+        locationManager.requestLocation()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
+        recogniser = NSClickGestureRecognizer(target: self, action: #selector(locationClicked))
+        recogniser?.numberOfClicksRequired = 1
+        mapView.addGestureRecognizer(recogniser!)
+    }
+    
+    @objc func locationClicked() {
+        guard let mouseClickLocation = recogniser?.location(in: mapView) else { return }
+        let coordinate = mapView.convert(mouseClickLocation, toCoordinateFrom: mapView)
+        setAddress(coordinate: coordinate)
+    }
+    
+    func setAddress(coordinate: CLLocationCoordinate2D) {
+        let geocoder = CLGeocoder()
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        geocoder.reverseGeocodeLocation(location) { (placemark, error) in
+            if error == nil {
+                let placemark = placemark?.first
+                let address = placemark?.name
+                DispatchQueue.main.async {
+                    self.addressLabel.stringValue = address ?? ""
+                }
+            }
+        }
     }
 
     override var representedObject: Any? {
@@ -21,7 +55,19 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        guard let coordinate = location?.coordinate else { return }
+        
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        mapView.setRegion(region, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
