42c3a7ae05425cf6828ffaf2efd6554bd34346d7
mithunraj <mithun.raj159@gmail.com>
2020-09-12 13:10:13 +0530

    Initial Commit

---
+++ MapLocationApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapLocationApp
+//
+//  Created by Mithun Raj on 12/09/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
